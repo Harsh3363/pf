9d87bcc85f266121fc8825f06c8f0314946676be
Harsh Kanodiya <harsh.kanodiya@gmail.com>
2022-10-09 12:37:39 +0530

    removed link placed with a tag

--- src/components/Navbar/Menus.jsx
+++ src/components/Navbar/Menus.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import { Link } from 'react-scroll'
+// import { Link } from 'react-scroll'
 
 
 export default function Menus() {
   return (
     <>
       <div className="w-full">
-        <Link to="community" spy={true} smooth={true} offset={50} duration={500} href="/" className='hover:text-primary py-2 block'>RoadMap</Link>
+        <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
       </div>
       {/* <div className="w-full">
         <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
       </div> */}
       <div className="w-full">
-        <Link to="community" spy={true} smooth={true} offset={50} duration={500} href="/" className='hover:text-primary py-2 block'>Documentation</Link>
+        <a href="/" className='hover:text-primary py-2 block'>Documentation</a>
       </div>
       <div className="w-full">
-        <Link to="community" spy={true} smooth={true} offset={50} duration={500} href="/" className='hover:text-primary py-2 block'>Tools</Link>
+        <a href="/" className='hover:text-primary py-2 block'>Tools</a>
       </div>
       <div className="w-full">
-        <Link to="community" spy={true} smooth={true} offset={50} duration={500} href="/" className='hover:text-primary py-2 block'>App</Link>
+        <a href="/" className='hover:text-primary py-2 block'>App</a>
       </div>
       <div className="w-full">
-        <Link to="community" spy={true} smooth={true} offset={50} duration={500} href="/" className='hover:text-primary py-2 block'>Community</Link>
+        <a href="/" className='hover:text-primary py-2 block'>Community</a>
       </div>
         <div className="w-full">
-          <Link to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx" className='hover:text-primary py-2 block'>DEX</Link>
+          <a href="" className='hover:text-primary py-2 block'>DEX</a>
         </div>
     
         <div className="w-full">
-          <Link to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx" className='hover:text-primary py-2 block'>Casino</Link>
+          <a   href="" className='hover:text-primary py-2 block'>Casino</a>
         </div>
     
         <div className="w-full">
-          <Link to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx" className='hover:text-primary py-2 block'>FAQ's</Link>
+          <a  href="" className='hover:text-primary py-2 block'>FAQ's</a>
         </div>
     
     </>
   );
 }
+
+//  to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx"
