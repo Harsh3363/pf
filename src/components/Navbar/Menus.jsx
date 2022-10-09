43a16bc0b5e921fce316f28f4d7cff31ff952c74
Harsh Kanodiya <harsh.kanodiya@gmail.com>
2022-10-09 13:53:38 +0530

    removed link placed with a tag

--- src/components/Navbar/Menus.jsx
+++ src/components/Navbar/Menus.jsx
@@ -1,77 +1,42 @@
-// import React from 'react';
-// // import { Link } from 'react-scroll'
-
-
-// export default function Menus() {
-//   return (
-//     <>
-//       <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
-//       </div>
-//       {/* <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
-//       </div> */}
-//       <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>Documentation</a>
-//       </div>
-//       <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>Tools</a>
-//       </div>
-//       <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>App</a>
-//       </div>
-//       <div className="w-full">
-//         <a href="/" className='hover:text-primary py-2 block'>Community</a>
-//       </div>
-//         <div className="w-full">
-//           <a href="" className='hover:text-primary py-2 block'>DEX</a>
-//         </div>
-    
-//         <div className="w-full">
-//           <a   href="" className='hover:text-primary py-2 block'>Casino</a>
-//         </div>
-    
-//         <div className="w-full">
-//           <a  href="" className='hover:text-primary py-2 block'>FAQ's</a>
-//         </div>
-    
-//     </>
-//   );
-// }
-
-// //  to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx"
-
-
 import React from 'react';
+// import { Link } from 'react-scroll'
 
-// import DropdownMenu from './DropdownMenu';
 
 export default function Menus() {
   return (
     <>
       <div className="w-full">
-        <a href="/" className='hover:text-primary py-2 block'>Cryptocurrency</a>
+        <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
+      </div>
+      {/* <div className="w-full">
+        <a href="/" className='hover:text-primary py-2 block'>RoadMap</a>
+      </div> */}
+      <div className="w-full">
+        <a href="/" className='hover:text-primary py-2 block'>Documentation</a>
       </div>
       <div className="w-full">
-        <a href="/" className='hover:text-primary py-2 block'>Exchanges</a>
+        <a href="/" className='hover:text-primary py-2 block'>Tools</a>
       </div>
       <div className="w-full">
-        <a href="/" className='hover:text-primary py-2 block'>Watchlist</a>
+        <a href="/" className='hover:text-primary py-2 block'>App</a>
       </div>
       <div className="w-full">
-        <a href="/" className='hover:text-primary py-2 block'>NFT</a>
+        <a href="/" className='hover:text-primary py-2 block'>Community</a>
       </div>
-      <div className="w-full">
-        <a href="/" className='hover:text-primary py-2 block'>Portofolios</a>
-      </div>
-      {/* <DropdownMenu>
         <div className="w-full">
-          <a href="/" className='hover:text-primary py-2 block'>Portofolios</a>
+          <a href="" className='hover:text-primary py-2 block'>DEX</a>
         </div>
+    
         <div className="w-full">
-          <a href="/" className='hover:text-primary py-2 block'>Portofoliosx</a>
+          <a   href="" className='hover:text-primary py-2 block'>Casino</a>
         </div>
-      </DropdownMenu> */}
+    
+        <div className="w-full">
+          <a  href="" className='hover:text-primary py-2 block'>FAQ's</a>
+        </div>
+    
     </>
   );
 }
+
+//  to="faq" spy={true} smooth={true} offset={50} duration={1000}  href="../sections//FaqSection.jsx"
